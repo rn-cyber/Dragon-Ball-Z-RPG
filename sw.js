18a5c287135dfd0aa04b7bec78332cab0a0407b1
rn-cyber <rhyanrocha2321@gmail.com>
2026-07-27 20:39:44 -0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('meu-jogo-cache1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/script.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
